2a80b36a4076a91965e1f2a657a2ae8b7aa5aaa0
Szymon Kolber <kolberszymon@gmail.com>
2022-06-29 20:47:14 +0800

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/components/**/*.{ts,tsx,js,jsx}', 
+    './src/pages/**/*.{ts,tsx,js,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
